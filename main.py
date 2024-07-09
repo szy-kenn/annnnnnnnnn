16af5defe3bf0a7f4393434be909ce11780af552
Ken <johnkennethvelano@gmail.com>
2024-07-09 21:29:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ with open("training.csv", "r") as f:
         inputs.append(input_row)
         targets.append(int(row[-1]))
 
-# print(inputs)
 
 def preprocessing(inputs):
     row = [0] * 7
@@ -26,10 +25,13 @@ def preprocessing(inputs):
     row.extend(col)
     return row
 
+_neuron_count = 5
+_fan_in = 2
+
 def train():
           
-        model = ANN(neuron_count=5, 
-                    fan_in=2, 
+        model = ANN(neuron_count=_neuron_count, 
+                    fan_in=_fan_in, 
                     fan_out=1)
 
         weights, biases = model.train(inputs, targets,
@@ -47,8 +49,8 @@ def train():
                 f.write(str(biases))
 
 def test():
-    trained = ANN(neuron_count=5, 
-            fan_in=2, 
+    trained = ANN(neuron_count=_neuron_count, 
+            fan_in=_fan_in, 
             fan_out=1,     
             weights=[[0.3917949289258904, 0.6715173880152004, -0.21166152414942513, 0.14835417268097964], [1.0260756002490465, 0.9851282930568723], [0.9770666139261482, 1.0130796067452523], [0.9737024307433648, 1.014998302611101], [0.9940179784191709, 1.003411728628598]],
             biases=[-2.6696000838016847e-05, -0.008654184305581115, 0.007611320465822845, 0.008727853202305836, 0.0019853624379129035]
